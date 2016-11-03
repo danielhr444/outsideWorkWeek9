4dbe1416deaeb05cd5329ab6014e875a4227a901
danielhr444 <danielhr444@gmail.com>
2016-11-02 17:09:36 -0700

    first commit

---
+++ Stuff.cpp
@@ -0,0 +1,18 @@
+/*
+ * Stuff.cpp
+ *
+ *  Created on: Nov 2, 2016
+ *      Author: danie
+ */
+
+#include "Stuff.h"
+
+Stuff::Stuff() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Stuff::~Stuff() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Stuff.h
@@ -0,0 +1,17 @@
+/*
+ * Stuff.h
+ *
+ *  Created on: Nov 2, 2016
+ *      Author: danie
+ */
+
+#ifndef STUFF_H_
+#define STUFF_H_
+
+class Stuff {
+public:
+	Stuff();
+	virtual ~Stuff();
+};
+
+#endif /* STUFF_H_ */
